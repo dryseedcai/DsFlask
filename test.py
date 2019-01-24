fe05c3f4460717c837958724b5050150d7288a99
caiminming <caiminming@qiyi.com>
2019-01-24 18:48:08 +0800

    c2 - 路由设置 & 配置文件

--- test.py
+++ test.py
@@ -4,12 +4,18 @@ __author__ = "DS"
 
 app = Flask(__name__)
 
-app.run()
+
+@app.route('/Hello/')
+def hello():
+    return 'Hello, DS'
 
 
+# 另一种路由注册方式
+# def hello2():
+#     return 'Hello2, DS'
+# app.add_url_rule('/Hello2/', view_func=hello2)
 
+# 引入flask配置文件
+app.config.from_object('config')
 
-
-
-
-
+app.run(debug=app.config['DEBUG'])
